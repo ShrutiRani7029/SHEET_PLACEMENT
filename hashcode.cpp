82f9ced545a4ec694f36288b7661534249d8252b
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-02-14 01:47:02 +0530

    Add files via upload

---
+++ hashcode.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k,m,p;
+    cin>>n>>k;
+   // map<string,int>mp;
+   string s,r;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s>>m;
+       // mp[s]++;
+       for(int j=0;j<m;j++)
+       {
+           cin>>r>>p;
+       }
+
+
+    }
+    string s1,s2;
+    int a,b,c,d,e;
+    for(int i=0;i<k;i++)
+    {
+        cin>>s1>>a>>b>>c>>d;
+        for(int i=0;i<d;i++)
+        {
+            cin>>s2>>e;
+        }
+
+    }
+
+
+}
