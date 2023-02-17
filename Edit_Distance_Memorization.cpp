8aa4feb74df3fff54741effb8ed19f7f39c157f4
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-02-17 22:37:15 +0530

    T.C - O(n*m), S.C - O(n*m) + O(n+m)
    problem link: https://leetcode.com/problems/edit-distance/description/

---
+++ Edit_Distance_Memorization.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp)
+{
+    ///base case
+    if(i==0)return j;
+    if(j==0)return i;
+
+    if(dp[i][j]!=-1)return dp[i][j];
+
+    if(s1[i-1]==s2[j-1])return dp[i][j] = 0+f(i-1,j-1,s1,s2,dp);
+    return dp[i][j] = 1+min(f(i,j-1,s1,s2,dp),min(f(i-1,j,s1,s2,dp),f(i-1,j-1,s1,s2,dp)));
+}
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,word1,word2,dp);
+    }
+};
