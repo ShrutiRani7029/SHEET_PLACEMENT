de89107d9da64742dcca008ab3dceb12d3ff1789
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-02-17 22:18:09 +0530

    Create distinct_subsequences.cpp

---
+++ distinct_subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));//cant be int
+
+        ///base condition
+        for(int i=0;i<=n;i++)dp[i][0]=1;//nothing left in second string
+        //for(int j=1;j<=m;j++)dp[0][j]=1;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        return (int)dp[n][m]; ///typecasting
+
+    }
+};
