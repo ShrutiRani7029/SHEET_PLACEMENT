0bdcfaf849c783dc4e060d890161348884a673a1
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-03-09 23:22:25 +0530

    o(n)-t.c, o(n)-s.c

---
+++ pascal_triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+        int n=numRows;
+        
+        for(int i=0;i<n;i++)
+        {
+            vector<int>row(i+1,1);
+            for(int j=1;j<i;j++)
+            {
+                row[j]=ans[i-1][j]+ans[i-1][j-1];
+            }
+            ans.push_back(row);
+        }
+        return ans;
+        
+        
+    }
+};
