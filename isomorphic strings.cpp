62c507b36eda2d16ffd01db89e1518f9a2816179
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-03-31 23:12:48 +0530

    Create isomorphic strings.cpp

---
+++ isomorphic strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false;
+        
+        unordered_map<char, char> mp1;
+        unordered_map<char, char> mp2;
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(mp1.find(s[i]) != mp1.end() && mp1[s[i]] != t[i]) {
+                return false;
+            }
+            if(mp2.find(t[i]) != mp2.end() && mp2[t[i]] != s[i]) {
+                return false;
+            }
+            mp1[s[i]] = t[i];
+            mp2[t[i]] = s[i];
+        }
+        
+        return true;
+    
+    }
+};
