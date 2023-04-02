e0feab28772081d29e925409d993900ef8c62a93
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-04-02 22:09:47 +0530

    Create guess number higher or lower.cpp

---
+++ guess number higher or lower.cpp
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int start=1,end=n;
+        
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            int val=guess(mid);
+            if(val==0)return mid;
+            else if(val==-1)end=mid-1;
+            else  start=mid+1;
+        }
+        return -1;
+    }
+};
